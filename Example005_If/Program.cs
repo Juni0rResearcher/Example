d22e4c3251e0ffe1aadffb2cbfcc33b2e966114b
JuniorResearcher <andrei-yuzhanin@mail.ru>
2023-02-23 20:54:40 +0300

    edit example005

---
+++ Example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура! Это же Ее величетсво МАРИЯ!");
+}
+else
+{
+    Console.Write("Приветствую, ");
+    Console.WriteLine(username);
+}
