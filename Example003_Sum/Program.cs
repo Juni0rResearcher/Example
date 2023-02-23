1939306629eb1749d93e9f640530dca1daae16a7
JuniorResearcher <andrei-yuzhanin@mail.ru>
2023-02-23 18:25:22 +0300

    edit example003

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numberA = new Random().Next(1, 10); // 1 2 3 4..9
-Console.Write(numberA);
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
 int result = numberA+numberB;
