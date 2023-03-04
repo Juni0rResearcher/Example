b597d9ad5915795cf2477b9349aa055964962e0a
JuniorResearcher <andrei-yuzhanin@mail.ru>
2023-03-04 22:38:33 +0300

    git commit - m "Edit Example012_Methods"

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -41,14 +41,24 @@ int year = Method3();
 
 
 //Вид 4. Методы которые что-то принимают и что-то возвращают
+//string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
