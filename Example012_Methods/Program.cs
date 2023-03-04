73303a4f8edc84de0f78a7549dc73f645d629181
JuniorResearcher <andrei-yuzhanin@mail.ru>
2023-03-04 23:40:54 +0300

    New cycle

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -64,5 +64,61 @@ string Method4(int count, string text)
 }
 
 string res = Method4(10, "Я уеду в Комарово ");
-System.Console.WriteLine(res);
+// System.Console.WriteLine(res);
 
+string text = "- Я думаю, - сказал Лосяш, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace (string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i =0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+// string newText = Replace(text, ' ', '|');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+// newText = Replace(text, 'к', 'К');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+// newText = Replace(text, 'с', 'С');
+// System.Console.WriteLine(newText);
+
+
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
